9bb3741859e85af82bce457ed826d74fe776c253
uxchar <chauncey.harlan.1@gmail.com>
2024-11-20 23:49:32 -0500

    updated imgs paths

--- src/components/PhotoFeed.vue
+++ src/components/PhotoFeed.vue
@@ -4,7 +4,7 @@ import PhotoCard from "./PhotoCard.vue";
 
 const photos = [
   {
-    localSrc: "src/assets/images/adam-borkowski-n5MeAdr8UAo-unsplash.jpg",
+    localSrc: "./src/assets/images/adam-borkowski-n5MeAdr8UAo-unsplash.jpg",
     webSrc:
       "https://unsplash.com/photos/a-person-walking-across-a-bridge-at-sunset-n5MeAdr8UAo",
     alt: "man walking across bridge",
@@ -12,14 +12,14 @@ const photos = [
     tag: "city",
   },
   {
-    localSrc: "src/assets/images/bruno-aguirre-CLmYbo-btDs-unsplash.jpg",
+    localSrc: "./src/assets/images/bruno-aguirre-CLmYbo-btDs-unsplash.jpg",
     webSrc: "https://unsplash.com/photos/silhouette-of-buildings-CLmYbo-btDs",
     alt: "sunset in a large city",
     creator: "Bruno Aguirre",
     tag: "city",
   },
   {
-    localSrc: "src/assets/images/gabriel-miklos-CIxr7iuT8-Q-unsplash.jpg",
+    localSrc: "./src/assets/images/gabriel-miklos-CIxr7iuT8-Q-unsplash.jpg",
     webSrc:
       "https://unsplash.com/photos/a-person-walking-down-a-street-CIxr7iuT8-Q",
     alt: "person walking down the street",
@@ -27,7 +27,7 @@ const photos = [
     tag: "city",
   },
   {
-    localSrc: "src/assets/images/marcus-dall-col-XU-mMDweXR4-unsplash.jpg",
+    localSrc: "./src/assets/images/marcus-dall-col-XU-mMDweXR4-unsplash.jpg",
     webSrc:
       "https://unsplash.com/photos/silhouette-of-cloud-with-sunlight-XU-mMDweXR4",
     alt: "silhouette of cloud with sunlight",
@@ -35,7 +35,7 @@ const photos = [
     tag: "nature",
   },
   {
-    localSrc: "src/assets/images/casey-horner-UPIojcqDFfo-unsplash.jpg",
+    localSrc: "./src/assets/images/casey-horner-UPIojcqDFfo-unsplash.jpg",
     webSrc:
       "https://unsplash.com/photos/silhouette-of-city-during-sunset-UPIojcqDFfo",
     alt: "silhouette of city during sunset",
@@ -43,7 +43,7 @@ const photos = [
     tag: "city",
   },
   {
-    localSrc: "src/assets/images/amogh-manjunath-HksFlo1t8iA-unsplash.jpg",
+    localSrc: "./src/assets/images/amogh-manjunath-HksFlo1t8iA-unsplash.jpg",
     webSrc:
       "https://unsplash.com/photos/grey-cable-car-between-buildings-HksFlo1t8iA",
     alt: "grey cable car between buildings",
@@ -51,7 +51,7 @@ const photos = [
     tag: "city",
   },
   {
-    localSrc: "src/assets/images/elsa-guyader-q8qFaWNOHRM-unsplash.jpg",
+    localSrc: "./src/assets/images/elsa-guyader-q8qFaWNOHRM-unsplash.jpg",
     webSrc:
       "https://unsplash.com/photos/silhouette-of-trees-during-sunset-q8qFaWNOHRM",
     alt: "silhouette of trees during sunset",
@@ -59,7 +59,7 @@ const photos = [
     tag: "nature",
   },
   {
-    localSrc: "src/assets/images/johannes-hurtig-z-fpG7D7buk-unsplash.jpg",
+    localSrc: "./src/assets/images/johannes-hurtig-z-fpG7D7buk-unsplash.jpg",
     webSrc:
       "https://unsplash.com/photos/aerial-photography-of-cityscape-z-fpG7D7buk",
     alt: "aerial photography of cityscape",
@@ -67,7 +67,7 @@ const photos = [
     tag: "city",
   },
   {
-    localSrc: "src/assets/images/adam-borkowski-SLFhsmMkW-A-unsplash.jpg",
+    localSrc: "./src/assets/images/adam-borkowski-SLFhsmMkW-A-unsplash.jpg",
     webSrc:
       "https://unsplash.com/photos/a-yellow-train-on-the-tracks-SLFhsmMkW-A",
     alt: "a yellow train on the tracks",
@@ -75,21 +75,22 @@ const photos = [
     tag: "city",
   },
   {
-    localSrc: "src/assets/images/daniele-levis-pelusi-jTknOGI18us-unsplash.jpg",
+    localSrc:
+      "./src/assets/images/daniele-levis-pelusi-jTknOGI18us-unsplash.jpg",
     webSrc: "https://unsplash.com/photos/photo-of-illuminated-city-jTknOGI18us",
     alt: "photo of illuminated city",
     creator: "Daniele Levis Pelusi",
     tag: "city",
   },
   {
-    localSrc: "src/assets/images/brady-cook-k1OIOwn1uJw-unsplash.jpg",
+    localSrc: "./src/assets/images/brady-cook-k1OIOwn1uJw-unsplash.jpg",
     webSrc: "https://unsplash.com/photos/sunset-over-the-horizon-k1OIOwn1uJw",
     alt: "sunset over the horizon",
     creator: "Brady Cook",
     tag: "city",
   },
   {
-    localSrc: "src/assets/images/biel-morro-d0xjEv-WJQk-unsplash.jpg",
+    localSrc: "./src/assets/images/biel-morro-d0xjEv-WJQk-unsplash.jpg",
     webSrc:
       "https://unsplash.com/photos/brown-cathedral-during-daytime-d0xjEv-WJQk",
     alt: "brown cathedral during daytime",
@@ -97,7 +98,7 @@ const photos = [
     tag: "city",
   },
   {
-    localSrc: "src/assets/images/rowan-heuvel-IrhXxv0g29I-unsplash.jpg",
+    localSrc: "./src/assets/images/rowan-heuvel-IrhXxv0g29I-unsplash.jpg",
     webSrc:
       "https://unsplash.com/photos/landscape-photography-of-silhouette-of-palm-trees-during-sunset-IrhXxv0g29I",
     alt: "landscape photography of silhouette of palm trees during sunset",
@@ -105,7 +106,7 @@ const photos = [
     tag: "nature",
   },
   {
-    localSrc: "src/assets/images/tim-marshall-bh75y-7eYVo-unsplash.jpg",
+    localSrc: "./src/assets/images/tim-marshall-bh75y-7eYVo-unsplash.jpg",
     webSrc:
       "https://unsplash.com/photos/black-rock-formation-on-sea-during-daytime-bh75y-7eYVo",
     alt: "black rock formation on sea during daytime",
@@ -113,7 +114,7 @@ const photos = [
     tag: "nature",
   },
   {
-    localSrc: "src/assets/images/zhan-zhang-Z_3ub5HQGjM-unsplash.jpg",
+    localSrc: "./src/assets/images/zhan-zhang-Z_3ub5HQGjM-unsplash.jpg",
     webSrc:
       "https://unsplash.com/photos/silhouette-of-flock-of-birds-flying-above-body-of-water-during-golden-hour-Z_3ub5HQGjM",
     alt: "silhouette of flock of birds flying above body of water during golden hour",
@@ -121,7 +122,7 @@ const photos = [
     tag: "nature",
   },
   {
-    localSrc: "src/assets/images/martino-pietropoli-5voYtW69x0Y-unsplash.jpg",
+    localSrc: "./src/assets/images/martino-pietropoli-5voYtW69x0Y-unsplash.jpg",
     webSrc:
       "https://unsplash.com/photos/worms-eye-view-photography-of-grey-mountain-5voYtW69x0Y",
     alt: "photography of grey mountain",
@@ -129,7 +130,7 @@ const photos = [
     tag: "nature",
   },
   {
-    localSrc: "src/assets/images/clay-banks-O5hfuVWgsS8-unsplash.jpg",
+    localSrc: "./src/assets/images/clay-banks-O5hfuVWgsS8-unsplash.jpg",
     webSrc:
       "https://unsplash.com/photos/silhouette-of-airplane-flying-over-the-people-during-sunset-O5hfuVWgsS8",
     alt: "silhouette of airplane flying over the people during sunset",
@@ -137,14 +138,14 @@ const photos = [
     tag: "city",
   },
   {
-    localSrc: "src/assets/images/lerone-pieters-Skfvy6yX1So-unsplash.jpg",
+    localSrc: "./src/assets/images/lerone-pieters-Skfvy6yX1So-unsplash.jpg",
     webSrc: "https://unsplash.com/photos/bridge-Skfvy6yX1So",
     alt: "bridge in new york city",
     creator: "Lerone Pieters",
     tag: "city",
   },
   {
-    localSrc: "src/assets/images/yiran-ding-YyhXBQw4skk-unsplash.jpg",
+    localSrc: "./src/assets/images/yiran-ding-YyhXBQw4skk-unsplash.jpg",
     webSrc:
       "https://unsplash.com/photos/woman-sitting-on-top-of-building-YyhXBQw4skk",
     alt: "woman sitting on top of building",
@@ -152,7 +153,7 @@ const photos = [
     tag: "city",
   },
   {
-    localSrc: "src/assets/images/lerone-pieters-vF6mSAWAzzU-unsplash.jpg",
+    localSrc: "./src/assets/images/lerone-pieters-vF6mSAWAzzU-unsplash.jpg",
     webSrc:
       "https://unsplash.com/photos/birds-eye-view-of-buildings-vF6mSAWAzzU",
     alt: "bird's eye view of buildings",
